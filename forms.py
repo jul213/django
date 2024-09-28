c49e6b4f51e826be254af2eabb53815370e15949
jul213 <158296107+jul213@users.noreply.github.com>
2024-09-28 19:04:08 +0200

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,2 +1,10 @@
 puedes hacer formularios con django teniendo una forma similar a los modelos solo que se renderiza por una plantilla y por la url
 tambien utilizando funciones 
+
+from django import forms
+
+class CommentForm(forms.Form):
+    name = forms.CharField(label="Escribe tu nombre")
+    url = forms.URLField(label="tu sitio web", required=False)
+    correo = forms.EmailField(label="coloca tu correo electronico")
+    
